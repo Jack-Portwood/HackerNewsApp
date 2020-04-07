2569be35fdc1eda30aa198683a49a9b36e14a1c5
Jack Portwood <Jack.Portwood14@gmail.com>
2020-04-07 11:18:29 +0100

    app.js complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import StoryContainer from './containers/StoryContainer';
 
-function App() {
+class App extends Component {
+  render(){
+    const stories = [
+      { name: "Lastest Stories", url:"https://hacker-news.firebaseio.com/v0/topstories.json"},
+      { name: "Story Detail", url:"https://hacker-news.firebaseio.com/v0/item/{storyId}.json"}
+    ]
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoryContainer stories={stories}/>
   );
+  }
 }
 
 export default App;
